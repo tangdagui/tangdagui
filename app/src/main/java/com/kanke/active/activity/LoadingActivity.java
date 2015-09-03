410253151fb0f55984bbd1a9fcb720840ad21909
dagui <1533927808@qq.com>
2015-09-03 15:11:30 +0800

    New changelist

--- app/src/main/java/com/kanke/active/activity/LoadingActivity.java
+++ app/src/main/java/com/kanke/active/activity/LoadingActivity.java
@@ -1,7 +1,26 @@
 package com.kanke.active.activity;
 
+import android.os.Bundle;
+
+import com.kanke.active.base.BaseActivity;
+
 /**
  * Created by Administrator on 2015/9/3.
  */
-public class LoadingActivity {
+public class LoadingActivity extends BaseActivity {
+    @Override
+    public void LoadingData() {
+
+    }
+
+    @Override
+    public int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
+
